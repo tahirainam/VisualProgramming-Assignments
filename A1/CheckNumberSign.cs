254924c0ea28438768fe3dc5819e893f7fa6f6ef
tahirainam <cattblackwhite@gmail.com>
2025-03-22 17:35:52 +0530

    Add project files.

---
+++ A1/CheckNumberSign.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VisualProgramming_Assignments.A1
+{
+    class CheckNumberSign
+    {
+        static void Main (string[] args)
+        {
+            int num;
+            Console.WriteLine("Check Number Sign");
+            Console.Write("Enter a Number: ");
+            num = Console.Read();
+
+            if(num == 0)
+            {
+                Console.WriteLine("A Zero");
+            }
+            else if(num > 0)
+            {
+                Console.WriteLine("A Positive Number");
+            }
+            else
+            {
+                Console.WriteLine("A Negative Number");
+            }
+        }
+    }
+}
---
+++ A1/FactorialCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualProgramming_Assignments.A1
+{
+    class FactorialCalculator
+    {
+    }
+}
