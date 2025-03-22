569538bb0b99e8eb5650158afc559c74e55f0266
tahirainam <cattblackwhite@gmail.com>
2025-03-23 00:54:58 +0530

    Added min max Finder

--- A1/MinMaxFinder.cs
+++ A1/MinMaxFinder.cs
@@ -8,5 +8,32 @@ namespace VisualProgramming_Assignments.A1
 {
     class MinMaxFinder
     {
+        static void Main()
+        {
+            int[] arr = new int[10] ;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter number for index {i}: ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int min = arr[0], max = arr[0];
+            int minIndx = 0, maxIndx = 0;
+            for (int i = 0; i < 10; i++)
+            {
+               if (arr[i] > max)
+                {
+                    max = arr[i];
+                    maxIndx = i;
+                }
+               if (arr[i] < min)
+                {
+                    min = arr[i];
+                    minIndx = i;
+                }
+            }
+
+            Console.WriteLine($"{max} is the Maximum Number at index {maxIndx}");
+            Console.WriteLine($"{min} is the Minimum Number at index {minIndx}");
+        }
     }
 }
