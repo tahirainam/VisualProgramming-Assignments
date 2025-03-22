e61b5df505d7f1460733cf81c18922ebe14856a4
tahirainam <cattblackwhite@gmail.com>
2025-03-22 17:47:23 +0530

    added Factorial Calculator

--- A1/FactorialCalculator.cs
+++ A1/FactorialCalculator.cs
@@ -8,5 +8,20 @@ namespace VisualProgramming_Assignments.A1
 {
     class FactorialCalculator
     {
+        static void Main(string[] args)
+        {
+            int num;
+            int fact=0;
+            Console.WriteLine("Factorial Calculator");
+            Console.Write("Enter a Number: ");
+            num = Console.Read();
+
+            for(int i=num; i>1; i--)
+            {
+                fact *= i;
+            }
+
+            Console.WriteLine($"Factorial of {num} is {fact}");
+        }
     }
 }
